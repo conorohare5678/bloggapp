3c1c865fd4d1524b43c278178f2e99dc6168e31a
conorohare5678 <conorohare5678@hotmail.com>
2025-01-23 18:16:09 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <Navbarheader/>
     <Routes>
       
-        <Route path="/Homepage" element={<HomePage/>} />
+        <Route path="/" element={<HomePage/>} />
         <Route path='/BloggPage' element={<BloggPage/>}/>
 
 </Routes>
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,10 +1,17 @@
 import React from "react"
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+
+    const navigate = useNavigate();
     return(
         <>
-        <h1>Blogging Page</h1>
-        <p>This is an application where a user can blogg about daily sport that they have done</p>
+        <h1 className="center-text">Blogging Page</h1>
+        <p className="center-text">This is an application where a user can blogg about daily sport that they have done</p>
+        <p className="center-text">A user can posta daily bog here to the page where they can login and post about their daily activity</p>
+        
+        <Button variant="info" className="center-item" onClick={() => navigate("/BloggPage")}>Lets get Blogging</Button>
         </>
     );
 }
