c3125ba3899df5f8404eea19c5ef52f052bb7635
conorohare5678 <conorohare5678@hotmail.com>
2025-01-29 20:32:26 +0000

    getting and axios statements

--- src/pages/bloggpage.js
+++ src/pages/bloggpage.js
@@ -1,35 +1,39 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import React from "react";
 import ModalButton from "../components/modal"
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import axios from "axios"
 
-const BloggPage = () =>{
-    // const [show, setShow] = useState(false);
+const BloggPage = () => {
 
-    // const handleClose = () => setShow(false);
-    // const handleShow = () => setShow(true);
-  
-    return(
-        <>
-        <ModalButton/>
-        <div>
-        <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+  const [blogg, setBlogg] = useState([]);
 
-        </div>
-      
-        </>
-    )
+  useEffect(()=>{
+    axios.get('http://localhost:5321/getBlog')
+    .then(blogg => setBlogg(blogg.data))
+    .catch(err => console.log(err))
+  }, [])
+
+  return (
+    <>
+      <ModalButton />
+      <div className="card-container">
+        {blogg.map (bloggDis => (
+        <Card style={{ width: '18rem', marginBottom: '10px' }}>
+          <Card.Body>
+            <Card.Title>{bloggDis.title}</Card.Title>
+            <Card.Text>
+              {bloggDis.content}
+            </Card.Text>
+            <Button variant="primary">View in pop up</Button>
+          </Card.Body>
+        </Card>
+))}
+      </div>
+
+    </>
+  )
 
 }
 
